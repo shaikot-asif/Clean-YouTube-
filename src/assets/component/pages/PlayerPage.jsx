1fac5f8011f19530e864922fb187d88671d4fba1
shaikot-asif <asif522666@gmail.com>
2024-03-26 03:35:51 +0600

    add some feature in player page

--- src/assets/component/pages/PlayerPage.jsx
+++ src/assets/component/pages/PlayerPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 import { useParams } from "react-router-dom";
 
@@ -6,14 +8,31 @@ import YouTube from "react-youtube";
 
 const PlayerPage = ({ playlists }) => {
   const { playlistId } = useParams();
+  const videoId = playlists[playlistId].playlistItems[0].contentDetails.videoId;
+  const [state, setState] = useState(videoId);
 
-  let videoId = playlists[playlistId].playlistItems[0].contentDetails.videoId;
-  console.log(videoId);
+  const onClickHandel = (data) => {
+    console.log(data);
+    setState(data);
+  };
+
   console.log(playlists[playlistId]);
 
   return (
     <div>
-      <YouTube videoId={videoId} />
+      <div>
+        <YouTube videoId={state} />
+        <div>
+          {playlists[playlistId].playlistItems.map((item) => (
+            <div key={item.title}>
+              <h5 onClick={() => onClickHandel(item.contentDetails.videoId)}>
+                {item.title}
+              </h5>
+              <img src={item.thumbnails.url} alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
